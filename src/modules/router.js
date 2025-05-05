a1cf6a30650b5eb895e248dcc27947e5ccc41961
AlexK <kivlaros@gmail.com>
2025-05-05 14:23:45 +0300

    feat: add compress and get-path

--- src/modules/router.js
+++ src/modules/router.js
@@ -2,10 +2,12 @@ import { changeDir } from "./fs/change-dir.js";
 import { list } from "./fs/list.js";
 import {readFileStream} from "./fs/read-file.js"
 import { create } from "./fs/create.js";
+import { getPath } from "./assist/get-path.js";
+import { compress } from "./zip/compress.js";
 
 export default async function router(comandText){
     const errorMessage = 'Invalid input'
-    const commandList = ['ls', 'cd', 'up', 'cat', 'add', 'rm', 'rn','cp','mv','.exit', 'os', 'hash', 'compress', 'decompress', 'clear'];
+    const commandList = ['ls', 'cd', 'up', 'cat', 'add', 'rm', 'rn','cp','mv','.exit', 'os', 'hash', 'compress', 'decompress', 'clear', 'test'];
     const comandTextArr = comandText.split(' ')
     if(commandList.includes(comandTextArr[0])){
         switch(comandTextArr[0]){
@@ -24,6 +26,12 @@ export default async function router(comandText){
                 break        
             case 'add':
                 await create(comandTextArr)
+                break
+            case 'test':
+                getPath(comandTextArr)
+                break   
+            case 'compress':
+                compress(comandTextArr)
                 break    
         }
     }else{
---
+++ src/modules/zip/compress.js
@@ -0,0 +1,22 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createBrotliCompress } from 'zlib';
+import { pipeline } from 'stream/promises';
+import { getPath } from '../assist/get-path.js';
+
+export async function compress(comandTextArr) {
+    const pathsObj = getPath(comandTextArr,false)
+    const readStream = createReadStream(pathsObj.source);
+    const writeStream = createWriteStream(pathsObj.dest);
+    const brotliCompress = createBrotliCompress();
+
+    try{
+        await pipeline(
+            readStream,
+            brotliCompress,
+            writeStream
+          );
+
+    }catch(err){
+        throw new Error(err.message)
+    }
+}
